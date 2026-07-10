3af43035a5778d9df848d2bacf18de26940a3a1d
Masoud Jafari <78048976+Masoud2025@users.noreply.github.com>
2026-07-10 19:45:48 +0330

    style: update hero video and visual enhancements

--- app/components/sections/Hero.tsx
+++ app/components/sections/Hero.tsx
@@ -5,24 +5,25 @@ export default function Hero() {
   const { t } = useLanguage();
 
   return (
-    <section className="flex flex-col items-center">
-      <video
-        autoPlay
-        muted
-        loop
-        playsInline
-        className="w-full max-w-[40rem] mx-auto"
-      >
-        <source src="/videos/coder.mp4" type="video/mp4" />
-      </video>
-      <div className="mx-auto max-w-5xl text-center mt-8">
+    <section className="flex flex-col items-center mt-32">
+       <div className="mx-auto max-w-5xl text-center mt-8">
         {/* Title */}
-        <h1 className="md:text-5xl font-black leading-tight tracking-tight  sm:text-6xl lg:text-4xl">
+        <h1 className=" text-4xl md:text-5xl font-black leading-tight tracking-tight  sm:text-6xl lg:text-4xl">
           {t.hero.head1}
           <br />
           {t.hero.head2}
         </h1>
       </div>
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="w-full max-w-[20rem] mx-auto"
+      >
+        <source src="/videos/programmernobg.webm" type="video/mp4" />
+      </video>
+     
     </section>
   );
 }
